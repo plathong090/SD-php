eb075c3cacd158c35d335891c0d3442f59267d24
Pla <prangthong250947@gmail.com>
2026-01-08 22:46:30 +0700

    hw

--- w9.php
+++ w9.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <title> การสร้างฟอร์ม </title>
+    <title>การสร้างฟอร์ม</title>
 </head>
 
-กำหนด name , sex -> radio button , job -> check box , add new department 4 lists -> list box
 <body>
     <form action="" method="post" name="form1" id="form1">
-        รหัสบัตรประชาชน : <input type="text" name="IDcard" maxlength="13" /><br />
-        ชื่อ-นามสกุล : <input type="text" name="fullname" size="40" /><br />
-        เพศ : <select>
-            <option />ชาย
-            <option />หญิง
-        </select><br />
-        งานอดิเรก :
-        <select size="3" multiple>
-            <option /> อ่านหนังสือ
-            <option /> ดูโทรทัศน์
-            <option /> เล่นกีฬา
-        </select><br /><br />
-        <input type="submit" /><input type="reset" />
+
+        รหัสบัตรประชาชน :
+        <input type="text" name="IDcard" maxlength="13" /><br /><br />
+
+        ชื่อ-นามสกุล :
+        <input type="text" name="fullname" size="40" /><br /><br />
+
+        เพศ :
+        <input type="radio" name="gender" value="male" /> ชาย
+        <input type="radio" name="gender" value="female" /> หญิง
+        <br /><br />
+
+        งานอดิเรก : <br />
+        <input type="checkbox" name="hobby[]" value="read" /> อ่านหนังสือ <br />
+        <input type="checkbox" name="hobby[]" value="tv" /> ดูโทรทัศน์ <br />
+        <input type="checkbox" name="hobby[]" value="sport" /> เล่นกีฬา
+        <br /><br />
+
+        ภาควิชา :
+        <select name="department" size="4">
+            <option value="it">เทคโนโลยีสารสนเทศ</option>
+            <option value="IM">การจัดการอุตสาหกรรม</option>
+            <option value="CDM">การออกแบบและบริหารงานก่อสร้าง</option>
+            <option value="aei">วิศวกรรมเกษตรเพื่ออุตสาหกรรม</option>
+        </select>
+        <br /><br />
+
+        <input type="submit" value="ส่งข้อมูล" />
+        <input type="reset" value="ล้างข้อมูล" />
+
     </form>
 </body>
 
